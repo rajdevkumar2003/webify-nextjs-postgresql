86848c2d7a2cf3394145f0da1d66f2085abebb2f
Rajeev <rajeevkumarjbo2003@gmail.com>
2025-01-25 17:20:58 +0530

    added codeview loader

--- app/_components/CodeView.jsx
+++ app/_components/CodeView.jsx
@@ -12,37 +12,40 @@ import constants from "@/connects/constants";
 import axios from "axios";
 import { MessagesContext } from "@/context/MessagesContext";
 import prompts from "@/connects/prompts";
+import { LoaderPinwheel } from "lucide-react";
 const CodeView = () => {
   const [files, setFiles] = useState(constants?.DEFAULT_FILE);
   const [active, setActive] = useState("code");
-  const {messages}=useContext(MessagesContext);
+  const { messages } = useContext(MessagesContext);
+  const [codeGenerate, setCodeGenerate] = useState(false);
 
   useEffect(() => {
-      if (messages?.length > 0) {
-        const role = messages[messages?.length - 1].role;
-        if (role === "user") {
-          GenerateAiCode();
-        }
+    if (messages?.length > 0) {
+      const role = messages[messages?.length - 1].role;
+      if (role === "user") {
+        GenerateAiCode();
       }
-    }, [messages]);
-  
+    }
+  }, [messages]);
 
-  const GenerateAiCode=async()=>{
-    const PROMPT=JSON.stringify(messages)+" "+prompts.CODE_GEN_PROMPT;
-    const result=await axios.post('/api/gen-ai-code',{
-      prompt:PROMPT
+  const GenerateAiCode = async () => {
+    setCodeGenerate(true);
+    const PROMPT = JSON.stringify(messages) + " " + prompts.CODE_GEN_PROMPT;
+    const result = await axios.post("/api/gen-ai-code", {
+      prompt: PROMPT,
     });
 
     console.log(result.data);
-    const aiResp=result.data;
+    const aiResp = result.data;
 
-    const mergedFiles={...constants?.DEFAULT_FILE,...aiResp?.files};
+    const mergedFiles = { ...constants?.DEFAULT_FILE, ...aiResp?.files };
     setFiles(mergedFiles);
-  }
+    setCodeGenerate(false);
+  };
 
   return (
-    <div className="w-full max-h-[90vh] rounded-sm bg-blue-600 flex flex-col overflow-y-scroll overflow-x-scroll scrollbar-hide ">
-      <div className="flex p-1">
+    <div className="w-full max-h-[90vh] rounded-sm bg-blue-600 flex flex-col overflow-y-scroll overflow-x-scroll scrollbar-hide">
+     {codeGenerate===false?<><div className="flex p-1">
         <div className="flex p-1 gap-2 rounded-md border-blue-900 bg-blue-400">
           <h2
             onClick={() => setActive("code")}
@@ -58,40 +61,45 @@ const CodeView = () => {
           </h2>
         </div>
       </div>
-      <SandpackProvider 
-      className="overflow-y-scroll overflow-x-scroll"
-      template="react" 
-      files={files} 
-      customSetup={{
-        dependencies:{
-          ...constants.DEPENDANCY
-        }
-      }}
-      options={{
-        externalResources:['https://unpkg.com/@tailwindcss/browser@4']
-      }} >
-        <SandpackLayout className="w-full flex" style={{ display: "flex" }}>
-          {active === "code" && (
-            <div className="flex w-full">
-              <SandpackFileExplorer className="min-h-[83vh]" />
-              <SandpackCodeEditor
-                className="min-h-[83vh] md:min-w-[48vw] sm:min-w-[76vw] min-w-[66vw]"
-                showTabs
-                showLineNumbers={true}
-                showInlineErrors
-                wrapContent
-              />
-            </div>
-          )}
+        <SandpackProvider
+          className="overflow-y-scroll overflow-x-scroll"
+          template="react"
+          files={files}
+          customSetup={{
+            dependencies: {
+              ...constants.DEPENDANCY,
+            },
+          }}
+          options={{
+            externalResources: ["https://unpkg.com/@tailwindcss/browser@4"],
+          }}
+        >
+          <SandpackLayout className="w-full flex" style={{ display: "flex" }}>
+            {active === "code" && (
+              <div className="flex w-full overflow-x-scroll overflow-y-scroll scrollbar-hide">
+                <SandpackFileExplorer className="min-h-[83vh] scrollbar-hide" />
+                <SandpackCodeEditor
+                  className="min-h-[83vh] md:min-w-[48vw] sm:min-w-[76vw] min-w-[66vw] scrollbar-hide"
+                  showTabs
+                  showLineNumbers={true}
+                  showInlineErrors
+                  wrapContent
+                />
+              </div>
+            )}
 
-          {active === "preview" && (
-            <SandpackPreview
-              showNavigator={true}
-              className="min-h-[83vh] md:min-w-[57vw] sm:min-w-[76vw] min-w-[66vw]"
-            />
-          )}
-        </SandpackLayout>
-      </SandpackProvider>
+            {active === "preview" && (
+              <SandpackPreview
+                showNavigator={true}
+                className="min-h-[83vh] md:min-w-[57vw] sm:min-w-[76vw] min-w-[66vw]"
+              />
+            )}
+          </SandpackLayout>
+        </SandpackProvider></>
+      :<div className={`min-h-full justify-center items-center bg-black z-10 bg-transparent flex gap-2`}>
+        <LoaderPinwheel className="animate-spin" />Generating Code...
+      </div>
+      }
     </div>
   );
 };
